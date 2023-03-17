7a189115b91fcde47b52d041d191079184479319
Bhaskar-Rao-1 <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-03-17 11:19:07 +0530

    Add files via upload

---
+++ variables in c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    //printing variables 
+    int age=25;
+    char star='*';
+    float price=90.0;
+    double salary=1000.000;
+    
+    printf("age is %d\n",age);
+    printf("price is %f\n",price);
+    printf("satr is %c\n",star);
+    printf("salary is %lf", salary);
+    
+    
+    return 0;
+}
